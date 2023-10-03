a4e091cedb6b5e78e23d5c9b06bd9a910e991f66
leunga3atwit <leunga3@wit.edu>
2023-10-03 13:48:11 -0400

    Code
    Work

---
+++ src/COMP1050Section5.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Adrien
+ *
+ */
+public class COMP1050Section5 {
+
+	/**
+	 * 
+	 */
+	public COMP1050Section5() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
